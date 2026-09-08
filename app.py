0d3a8b9ecc14b5be2b08c439292111be2bdbd726
Kristin Terrill <kterrill@iastate.edu>
2026-09-08 11:40:14 -0500

    added listener for host = '0.0.0.0'

--- app.py
+++ app.py
@@ -34,5 +34,5 @@ init_opportunity_search_results_route(app)
 #3 Start the server if this file is run directly
 if __name__ == '__main__': # this means that if we run this file, the Flask server will start. If we import this file into another file, the Flask server will not start.
     port = int(os.environ.get("PORT", 5000)) # get the port from the environment variable PORT, or use 5000 if it's not set
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False) # run the Flask server on all available IP addresses 
     
